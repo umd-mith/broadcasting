f5eb39ae5b05b27527f1a005c63deaffe80cf387
Raff Viglianti <raffaeleviglianti@gmail.com>
2022-04-25 16:05:57 -0400

    header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,15 +2,20 @@ import { Link } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
 import React from "react"
 import Nav from "./nav"
+import "./header.css"
 
 const Header = () => {
   return (
     <header>
       <div className="header-content">
-        <Link aria-label="Broadcasting A/V data home page" to="/">
-          <StaticImage src="../images/logo.png" alt="Broadcasting A/V Data:
-          Using linked data and archival records to enhance discoverability for broadcasting collections"/>
-        </Link>
+        <div className="gradient">
+          <Link aria-label="Broadcasting A/V data home page" to="/">
+            <StaticImage src="../images/logo.png" alt="Broadcasting A/V Data:
+            Using linked data and archival records to enhance discoverability for broadcasting collections"
+            width={1000}
+            />
+          </Link>
+        </div>
         <Nav />
       </div>
     </header>
