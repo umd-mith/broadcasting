e6959abb8e918c4da3aa6eec56d82c7b6b0dce73
raffazizzi <raffaeleviglianti@gmail.com>
2023-03-29 11:00:46 -0400

    added clear button

--- src/components/search.tsx
+++ src/components/search.tsx
@@ -15,14 +15,21 @@ export const isSearchMatch = (query: string, string: string): boolean => {
 
 const Search = ({query, set, placeholder}: Props) => {
   return (
-    <input
-        className="search"
-        type="text"
-        value={query}
-        onChange={e => set(e.target.value)}
-        placeholder={placeholder}
-        aria-label="Search"
-      />
+    <div className="search">
+      <input
+          className="search-bar"
+          type="text"
+          value={query}
+          onChange={e => set(e.target.value)}
+          placeholder={placeholder}
+          aria-label="Search"
+          />
+      <span className="search-clear" onClick={() => set("")}>
+        <svg className="search-clear-icon" viewBox="10 10 30 30">
+          <path fillRule="evenodd" d="m12.45 37.65-2.1-2.1L21.9 24 10.35 12.45l2.1-2.1L24 21.9l11.55-11.55 2.1 2.1L26.1 24l11.55 11.55-2.1 2.1L24 26.1Z" clipRule="evenodd"/>
+        </svg>
+      </span>
+    </div>
   )
 }
 
